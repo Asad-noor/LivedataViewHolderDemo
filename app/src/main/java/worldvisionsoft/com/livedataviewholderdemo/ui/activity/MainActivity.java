aa68433e5e9348d7ecdb80f69bb6602627b4e447
Md. Asaduzzaman Noor <md.noor.asad@gmail.com>
2019-12-23 23:11:56 +0600

    Uncomment rxjava just example code.

--- app/src/main/java/worldvisionsoft/com/livedataviewholderdemo/ui/activity/MainActivity.java
+++ app/src/main/java/worldvisionsoft/com/livedataviewholderdemo/ui/activity/MainActivity.java
@@ -72,33 +72,33 @@ public class MainActivity extends AppCompatActivity {
         //getUniqueID();
         //makeEncrypt();
 
-//        Observable<String> observable = Observable.just("toto", "goto");
-//
-//        Observer<String> observer = new Observer<String>() {
-//            @Override
-//            public void onSubscribe(Disposable d) {
-//                Log.d("tttt", "onSubscribe");
-//            }
-//
-//            @Override
-//            public void onNext(String s) {
-//                Log.d("tttt", "onNext >" + s);
-//            }
-//
-//            @Override
-//            public void onError(Throwable e) {
-//                Log.d("tttt", "onError");
-//            }
-//
-//            @Override
-//            public void onComplete() {
-//                Log.d("tttt", "onComplete");
-//            }
-//        };
-//        observable.doOnNext(c -> Log.d("tttt", "do on Next"))
-//                .subscribeOn(Schedulers.io())
-//                .observeOn(AndroidSchedulers.mainThread())
-//                .subscribe(observer);
+        Observable<String> observable = Observable.just("toto", "goto");
+
+        Observer<String> observer = new Observer<String>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+                Log.d("tttt", "onSubscribe");
+            }
+
+            @Override
+            public void onNext(String s) {
+                Log.d("tttt", "onNext >" + s);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.d("tttt", "onError");
+            }
+
+            @Override
+            public void onComplete() {
+                Log.d("tttt", "onComplete");
+            }
+        };
+        observable.doOnNext(c -> Log.d("tttt", "do on Next"))
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(observer);
     }
 
     private void addHomeFragment() {
